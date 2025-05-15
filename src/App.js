c334b007a53746db44a82ed1511b69eba70e5432
EESEO <enrui2001@naver.com>
2025-05-16 00:32:59 +0900

    커밋 메시지 써주세요

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="text">
         content 추가해서 본인 이름 + 파트 작성하고 git에 브랜치 만들고 push하기
       </div>
-      <div className="content">임세연 백엔드</div>
+      <div className="content">이은서 백엔드</div>
       <div className="content">이민주 프론트엔드</div>
     </div>
   );
